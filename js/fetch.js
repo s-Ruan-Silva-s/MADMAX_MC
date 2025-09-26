b7a8aea814ae1ebcde9df562fd9227667871cbfd
Ruan Silva <ruan.s.abc@gmail.com>
2025-09-26 15:03:26 -0300

    carregamento assincrono de informações

---
+++ js/fetch.js
@@ -0,0 +1,11 @@
+document.querySelectorAll("button[data-page]").forEach(btn=>{
+    btn.addEventListener("click", async ()=>{
+
+        const page = btn.dataset.page;
+        fetch(page)
+            .then(e=>e.text())
+            .then(resp=>{
+                document.getElementById("content").innerHTML = resp;
+            })
+    })
+})
