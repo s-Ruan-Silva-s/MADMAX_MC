4c737b2ac0b2d60d3c2a68e7000f1b7f0746d0d9
Ruan Silva <ruan.s.abc@gmail.com>
2025-10-06 12:23:13 -0300

    add financial page

--- js/fetch.js
+++ js/fetch.js
@@ -1,11 +1,10 @@
-document.querySelectorAll("button[data-page]").forEach(btn=>{
-    btn.addEventListener("click", async ()=>{
-
-        const page = btn.dataset.page;
-        fetch(page)
-            .then(e=>e.text())
-            .then(resp=>{
-                document.getElementById("content").innerHTML = resp;
-            })
-    })
-})
+document.querySelectorAll("button[data-page]").forEach((btn) => {
+  btn.addEventListener("click", async () => {
+    const page = btn.dataset.page;
+    fetch(page)
+      .then((e) => e.text())
+      .then((resp) => {
+        document.getElementById("content").innerHTML = resp;
+      });
+  });
+});
